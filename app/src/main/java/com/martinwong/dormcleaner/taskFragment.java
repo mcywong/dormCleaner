efa47c92847090e5c6fb0f0dfaaf78f245c5c6d0
Martin Wong <martincywong08@gmail.com>
2016-05-12 00:55:40 -0400

    Implemented demo task list with listFragments, array adapter, and hardcoded string values in strings file

---
+++ app/src/main/java/com/martinwong/dormcleaner/taskFragment.java
@@ -0,0 +1,34 @@
+package com.martinwong.dormcleaner;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+/**
+ * Created by Martin Wong on 2016-05-12.
+ * taskFragment class for displaying a list of tasks to do
+ */
+public class taskFragment extends ListFragment implements AdapterView.OnItemClickListener{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.task_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.tasks, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getActivity(), "Item" + " " + position, Toast.LENGTH_SHORT).show();
+    }
+}
