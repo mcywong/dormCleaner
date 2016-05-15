4d205fd11f2d290cadc92796fe653ec318854eee
Martin Wong <martincywong08@gmail.com>
2016-05-15 00:18:06 -0400

    Implemented Navigation to detail activity from list view
    Added some icon assets for list items

--- app/src/main/java/com/martinwong/dormcleaner/taskFragment.java
+++ app/src/main/java/com/martinwong/dormcleaner/taskFragment.java
@@ -1,6 +1,7 @@
 package com.martinwong.dormcleaner;
 
 import android.app.ListFragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +15,7 @@ import android.widget.Toast;
  * taskFragment class for displaying a list of tasks to do
  */
 public class taskFragment extends ListFragment implements AdapterView.OnItemClickListener{
+    Intent detailSegue;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.task_fragment, container, false);
@@ -22,7 +24,8 @@ public class taskFragment extends ListFragment implements AdapterView.OnItemClic
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.tasks, android.R.layout.simple_list_item_1);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(
+                getActivity(), R.array.tasks, android.R.layout.simple_list_item_1);
         setListAdapter(adapter);
         getListView().setOnItemClickListener(this);
     }
@@ -30,5 +33,8 @@ public class taskFragment extends ListFragment implements AdapterView.OnItemClic
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         Toast.makeText(getActivity(), "Item" + " " + position, Toast.LENGTH_SHORT).show();
+        //make an intent and pass it into startActivity
+        detailSegue = new Intent(taskFragment.this.getActivity(), Task_Detail_Activity.class);
+        startActivity(detailSegue);
     }
 }
